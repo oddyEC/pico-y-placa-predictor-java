56bf6415f06dc7fc55af41131cbf8508484ed90c
Diego Márquez <57378696+oddyEC@users.noreply.github.com>
2022-07-15 00:07:17 -0500

    unit testing

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,20 +1,25 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
+import java.text.ParseException;
+
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+    String time = "09:21";
+    Car car1 = new Car("PBO-2217","09/07/2022",time);
+    Car car2 = new Car("PBX-0001","11/07/2022",time);
+    Car car3 = new Car("PZA-0003","12/07/2022",time);
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() throws ParseException {
+        assertFalse(car1.allowed_to_drive(time));
+        assertTrue(car2.allowed_to_drive(time));
+        assertTrue(car3.allowed_to_drive(time));
     }
 }
